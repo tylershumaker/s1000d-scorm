304c326a6400d96f1583f6e3a929ee9c2b19c6ff
shumaket <shumaket@ead9d327-ae1f-4177-9acf-1b888be707b0>
2010-11-16 20:35:46 +0000

    initial checkin

---
+++ test/bridge/toolkit/commands/PostProcessTest.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of the S1000D-SCORM Bridge Open Toolkit 
+ * project hosted on Sourceforge.net. See the accompanying 
+ * license.txt file for applicable licenses.
+ */
+package bridge.toolkit.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class PostProcessTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    /**
+     * Test method for {@link bridge.toolkit.commands.PostProcess#execute(org.apache.commons.chain.Context)}.
+     */
+    @Test
+    public void testExecute()
+    {
+        fail("Not yet implemented");
+    }
+
+}
