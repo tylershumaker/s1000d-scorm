846ca8ef706ecb71dfe596d0d0c1e8b0e26ed101
shumaket <shumaket@ead9d327-ae1f-4177-9acf-1b888be707b0>
2010-11-16 20:35:14 +0000

    initial checkin

---
+++ test/bridge/toolkit/packaging/ZipCreatorTest.java
@@ -0,0 +1,36 @@
+package bridge.toolkit.packaging;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ZipCreatorTest
+{
+   ZipCreator contentPackage;
+
+   @Before
+   public void setUp() throws Exception
+   {
+      contentPackage = new ZipCreator();
+   }
+
+   @After
+   public void tearDown() throws Exception
+   {
+   }
+
+   @Test
+   public void testZipFiles()
+   {
+      String packageLoc = System.getProperty("user.dir") + File.separator + "test_files\\Test_Bike_Package";
+      contentPackage.setPackageLocation(packageLoc);
+      contentPackage.zipFiles();
+   }
+
+}
