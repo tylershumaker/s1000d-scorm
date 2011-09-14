016884edb13a6dbbdb048c4b1e0943bfde1dcd71
yackot <yackot@ead9d327-ae1f-4177-9acf-1b888be707b0>
2011-09-14 16:22:10 +0000

    Replaced btnBack.disabled and btnNext.disabled with document.getElementById('btnBack').disabled and document.getElementById('btnNext').disabled respectively so that buttons would work in Chrome

--- branches/0.9.1_Features/ViewerApplication/app/navScript.js
+++ branches/0.9.1_Features/ViewerApplication/app/navScript.js
@@ -20,118 +20,115 @@ function get_location(name)
     var results = regex.exec(window.location.href);
     if (results == null) return ""; else return results[1];
 
- }
+}
 
 function startSCO()
-	{
-		var scoPages = getArray();
-		var newLocation = scoPages[loc][0];
-		parent.content.location=newLocation;
-		parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
-		// call course and sco initialize
-		scoInit();
-		//alert("This is the course and first sco page");
-	}
+{
+	var scoPages = getArray();
+	var newLocation = scoPages[loc][0];
+	parent.content.location=newLocation;
+	parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
+	// call course and sco initialize
+	scoInit();
+	//alert("This is the course and first sco page");
+}
 
-	function scoInit() 
-	{
-	    //trigger call to init the sco through the APIWrapper.js
-	    if (initialized == null) 
-	    {
-	        initialized = doInitialize();
-	        is_initialized = true;
-	    }
-	    btnBack.disabled = true;
-	    if (scoPages[loc].length == 1)
-	    {
-	    	btnNext.disabled = true;
-	    }
-	    if (scoPages[loc].length > 1)
-	    {
-	    	doSetValue("cmi.completion_status", "incomplete");
-	    }
-	}
+function scoInit() 
+{
+    //trigger call to init the sco through the APIWrapper.js
+    if (initialized == null) 
+    {
+        initialized = doInitialize();
+        is_initialized = true;
+    }
+    document.getElementById('btnBack').disabled = true;
+    if (scoPages[loc].length == 1)
+    {
+    	document.getElementById('btnNext').disabled = true;
+    }
+    if (scoPages[loc].length > 1)
+    {
+    	doSetValue("cmi.completion_status", "incomplete");
+    }
+}
 
-	function scoTerminate()
+function scoTerminate()
+{   
+//trigger call to terminate the sco through the APIWrapper.js
+    initialized = doTerminate();
+    is_initialized = false;
+    //alert(initialized);
+}
+
+function setStatus(status)
+{
+	if (status == "true")
 	{   
-	//trigger call to terminate the sco through the APIWrapper.js
-	    initialized = doTerminate();
-	    is_initialized = false;
-	    //alert(initialized);
-	}
-	
-	function setStatus(status)
-	{
-		if (status == "true")
-		{   
-		    //trigger call to set complete status the sco through the APIWrapper.js 
-		    if (is_initialized == true) {
-		        doSetValue("cmi.completion_status", "completed");
-		    }
-		}
-		else
-		{   
-		    //trigger call to set incomplete status the sco through the APIWrapper.js 
-		    if (is_initialized == true) {
-		        doSetValue("cmi.completion_status", "incomplete");
-		    }
-		}
-	}
-	
-	function goNext()
-	{
-	   // Only enable the back button if section is not an assessment
-	   var inString = scoPages[loc][1].indexOf("-T88");
-	   if (inString == -1)
-	   {
-		   btnBack.disabled = false;
-	   }
-
-	   if (count < scoPages[loc].length)
-	   {
-	       var nextPage = "";
-	       nextPage = scoPages[loc][count];
-	       count++;
-	       setStatus("false");
-	       parent.content.location=nextPage;  
-	   }
-	   if (count == scoPages[loc].length)
-	   {   
-		   setStatus("true");
-		   if (is_initialized != false)
-		   {
-			   scoTerminate();
-		   }
-	       btnNext.disabled = true;
-	   } 
-	   parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
-	}
-	
-	function goBack()
-	{
-        btnNext.disabled = false;
-		//btnNext.disabled = false;
-	    if (count > 1)
-	    {
-	    	count--;
-	    	var backPage = scoPages[loc][count-1];
-	        parent.content.location=backPage;
+	    //trigger call to set complete status the sco through the APIWrapper.js 
+	    if (is_initialized == true) {
+	        doSetValue("cmi.completion_status", "completed");
 	    }
-	    if (count == 1)
-	    {
-	    	btnBack.disabled = true;			
-	    }
-	    parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
 	}
-	
-	function goHome()
+	else
 	{   
-	    //btnNext.disabled = false;
-	    count = 1;
-	    var nextPage = scoPages[loc][count-1];
-	    parent.content.location=nextPage;
-	    parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
+	    //trigger call to set incomplete status the sco through the APIWrapper.js 
+	    if (is_initialized == true) {
+	        doSetValue("cmi.completion_status", "incomplete");
+	    }
 	}
+}
 
-	
+function goNext()
+{
+   // Only enable the back button if section is not an assessment
+   var inString = scoPages[loc][1].indexOf("-T88");
+   if (inString == -1)
+   {
+	   document.getElementById('btnBack').disabled = false;
+   }
 
+   if (count < scoPages[loc].length)
+   {
+       var nextPage = "";
+       nextPage = scoPages[loc][count];
+       count++;
+       setStatus("false");
+       parent.content.location=nextPage;  
+   }
+   if (count == scoPages[loc].length)
+   {   
+	   setStatus("true");
+	   if (is_initialized != false)
+	   {
+		   scoTerminate();
+	   }
+	   document.getElementById('btnNext').disabled = true;
+   } 
+   parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
+}
+
+function goBack()
+{
+    document.getElementById('btnNext').disabled = false;
+    if (count > 1)
+    {
+    	count--;
+    	var backPage = scoPages[loc][count-1];
+        parent.content.location=backPage;
+    }
+    if (count == 1)
+    {
+    	//btnBack.disabled = true;
+    	document.getElementById('btnBack').disabled = true;
+    }
+    parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
+}
+
+function goHome()
+{   
+    //btnNext.disabled = false;
+    count = 1;
+    var nextPage = scoPages[loc][count-1];
+    parent.content.location=nextPage;
+    parent.topframe.indexPage("Page " + count + " of " + scoPages[loc].length + "    ");
+}
