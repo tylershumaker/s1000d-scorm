1d3fcac120c0d400689905e97dba03de94281bcd
Richard Wolford <wolfordr@ctc.com>
2019-11-27 10:10:14 -0500

    Added ILPE Data and SCPM

--- test/bridge/toolkit/ControllerTest.java
+++ test/bridge/toolkit/ControllerTest.java
@@ -192,6 +192,14 @@ public class ControllerTest
             System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "ilpe_resource_slim_2",
             "-scormhtml"};
 
+        String[]  SCORM_ILPE_2004_Full = {System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "ILPE_SCPM_Full" + File.separator + "SMC-ILPE-06RT9-00001-00.xml",
+                System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "ILPE_resource_package_Full",
+                "-scormhtml"};
+
+        String[]  SCORM_ILPE_2004_Full_v2 = {System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "ILPE_SCPM_Full" + File.separator + "SMC-ILPE-06RT9-00001-00.xml",
+                System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "ILPE_resource_package_Full_v2",
+                "-scormhtml"};
+
         String[]  SCORM12_slim = {System.getProperty("user.dir")  + File.separator + "test_files" + File.separator + "scpm_slim" + File.separator + "SMC-S1000DBIKE-06RT9-00001-00.xml",
                 System.getProperty("user.dir") + File.separator + "test_files" + File.separator + "resource_package_slim",
                 "-SCORM12"};
@@ -207,6 +215,8 @@ public class ControllerTest
         //Controller.main(SCORM40HTMLWOUT);
         //Controller.main(SCORM12_slim);
         //Controller.main(SCORM12_slim_Zip_Resource);
-        Controller.main(SCORM_ILPE_2004);
+        //Controller.main(SCORM_ILPE_2004);
+        //Controller.main(SCORM_ILPE_2004_Full);
+        Controller.main(SCORM_ILPE_2004_Full_v2);
     }
 }
